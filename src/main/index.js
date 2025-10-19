8612e25b2fe46816b1971aa9b41bb5b0c78c7ed3
gpuawa <2547828133@qq.com>
2025-10-19 09:31:15 +0800

    upd: 分离 CSES 加载模块

--- src/main/index.js
+++ src/main/index.js
@@ -2,49 +2,18 @@
  * @author GPUawa
  * @since 2025/10/12
  * @license GPL-3.0
+ * @description 主进程
  */
 
 import { app, shell, BrowserWindow, ipcMain, screen, Menu, Tray } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { ScheduleLoader } from './CSESLoader.js'
 import icon from '../../resources/icon.png?asset'
-import fs from 'fs'
-import yaml from 'js-yaml'
 
 let scheduleLoader
 
-// 课表加载器类
-class ScheduleLoader {
-    constructor() {
-        this.configPath = join(__dirname, '../../configs/schedule.yaml')
-        this.loadSchedule()
-    }
-
-    loadSchedule() {
-        try {
-            this.scheduleData = yaml.load(fs.readFileSync(this.configPath, 'utf8'))
-        } catch (error) {
-            console.error('加载课表失败:', error)
-            this.scheduleData = this.getDefaultSchedule()
-        }
-    }
-
-    getWeekNumber(date) {
-        const janFirst = new Date(date.getFullYear(), 0, 1)
-        return Math.ceil(((date - janFirst) / 86400000 + janFirst.getDay() + 1) / 7)
-    }
-
-    getTodayClasses(date = new Date()) {
-        const dayIndex = date.getDay() || 7
-        const weekNum = this.getWeekNumber(date)
-        const weekType = weekNum % 2 === 0 ? 'even' : 'odd'
-
-        return this.scheduleData.schedules
-            .filter(s => s.enable_day === dayIndex && (s.weeks === 'all' || s.weeks === weekType))
-            .flatMap(s => s.classes)
-    }
-}
-
+// 创建窗口
 function createWindow() {
     const mainWindow = new BrowserWindow({
         x: 0,
@@ -67,9 +36,9 @@ function createWindow() {
     })
 
     mainWindow.on('ready-to-show', () => {
-        mainWindow.setAlwaysOnTop(1, 'normal')
-        mainWindow.setIgnoreMouseEvents(1)
-        mainWindow.setVisibleOnAllWorkspaces(1)
+        mainWindow.setAlwaysOnTop(true, 'normal')
+        mainWindow.setIgnoreMouseEvents(true)
+        mainWindow.setVisibleOnAllWorkspaces(true)
         mainWindow.show()
     })
 
@@ -85,6 +54,7 @@ function createWindow() {
     }
 }
 
+// 创建系统托盘
 function createTray() {
     const tray = new Tray(join(__dirname, '../../resources/icon.png'))
     const contextMenu = Menu.buildFromTemplate([
@@ -95,7 +65,7 @@ function createTray() {
     tray.setContextMenu(contextMenu)
 }
 
-// ipc
+// 注册 IPC 通信
 function registerIPC() {
     ipcMain.handle('schedule:getTodayClasses', (_, dateString) => {
         const date = dateString ? new Date(dateString) : new Date()
@@ -119,8 +89,4 @@ app.whenReady().then(() => {
 
     createWindow()
     createTray()
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
 })
